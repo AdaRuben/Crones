561b6f67f7e80e200753fdec509cf0ab1ca4eabf
Nastyusha <tor4ebes@mail.ru>
2025-11-10 13:22:52 +0300

    redis first realise

--- server/src/app.js
+++ server/src/app.js
@@ -6,6 +6,7 @@ const cookieParser = require('cookie-parser');
 
 const adminAuthRouter = require('./routes/adminAuth.routes');
 const orderRouter = require('./routes/order.routes');
+const AuthRouter = require('./routes/auth.router');
 
 const app = express();
 
@@ -17,5 +18,6 @@ app.use(express.static(path.join(__dirname, '..', 'public')));
 
 app.use('/api/auth', adminAuthRouter);
 app.use('/api/orders', orderRouter);
+app.use('/api/auth', AuthRouter);
 
 module.exports = app;
