f4881f32b1e76de7cec7b2c72adfb1e4d1b32e3a
Ada <plumsiren@mail.ru>
2025-11-12 15:03:34 +0300

    real

---
+++ admin/src/features/choiseDriver/ChoiceDriver.tsx
@@ -0,0 +1,65 @@
+import React, { useState } from 'react';
+import { Button, Modal, Space } from 'antd';
+
+function ChoiceDriver(): React.JSX.Element {
+  const [open, setOpen] = useState(false);
+
+  const showModal = () :void => {
+    setOpen(true);
+  };
+  const handleOk = () :void => {
+    setOpen(false);
+  };
+
+  const handleCancel = () :void => {
+    setOpen(false);
+  };
+  return (
+    <>
+      <Space>
+        <Button type="primary" onClick={showModal}>
+          Open Modal
+        </Button>
+        <Button
+          type="primary"
+          onClick={() => {
+            Modal.confirm({
+              title: 'Confirm',
+              content: 'Bla bla ...',
+              footer: (_, { OkBtn, CancelBtn }) => (
+                <>
+                  <Button>Custom Button</Button>
+                  <CancelBtn />
+                  <OkBtn />
+                </>
+              ),
+            });
+          }}
+        >
+          Open Modal Confirm
+        </Button>
+      </Space>
+      <Modal
+        open={open}
+        title="Title"
+        onOk={handleOk}
+        onCancel={handleCancel}
+        footer={(_, { OkBtn, CancelBtn }) => (
+          <>
+            <Button>Custom Button</Button>
+            <CancelBtn />
+            <OkBtn />
+          </>
+        )}
+      >
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+      </Modal>
+    </>
+  );
+};
+
+export default ChoiceDriver
