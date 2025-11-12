f40458eaa767a76874ce6c162afee82688c5ee2a
Nastyusha <tor4ebes@mail.ru>
2025-11-12 12:11:01 +0300

    nodemailer

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -3,7 +3,7 @@ import { store } from './store/store';
 
 // import MapPage from '../pages/MainPage/MainPage';
 import React from 'react';
-import AuthPage from '@/pages/Auth/AuthPage';
+// import AuthPage from '@/pages/Auth/AuthPage';
 
 
 import { BrowserRouter } from 'react-router';
@@ -14,7 +14,7 @@ export default function App(): React.JSX.Element {
   return (
     <Provider store={store}>
       {/* <MapPage /> */}
-      <AuthPage />
+      {/* <AuthPage /> */}
       <BrowserRouter>
         <AppRouter />
       </BrowserRouter>
